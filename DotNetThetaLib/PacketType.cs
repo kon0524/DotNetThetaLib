950db1fe14629793b7705b244a38baf0ccb6d0d4
kon0524 <sound.recording.man@gmail.com>
2015-09-19 09:45:56 +0900

    最初のコミット

---
+++ DotNetThetaLib/PacketType.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNetThetaLib
+{
+    public enum PacketType
+    {
+        InvalidValue = 0x00000000,
+        InitCommandRequest,
+        InitCommandAck,
+        InitEventRequest,
+        InitEventAck,
+        InitFail,
+        OperationRequest,
+        OperationResponse,
+        Event,
+        StartData,
+        Data,
+        Cancel,
+        EndData,
+        ProbeRequest,
+        ProbeResponse
+    }
+}
