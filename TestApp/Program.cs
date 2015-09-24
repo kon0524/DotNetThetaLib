2f57a6e5b524d91aecb25b08aa817955ba0abc3c
kon0524 <sound.recording.man@gmail.com>
2015-09-25 08:32:48 +0900

    テストアプリで画像を取得できるように対応した

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,9 +9,17 @@ namespace TestApp
     {
         private static string IP = "192.168.1.1";
         private static int PORT = 15740;
+        private static string APP_HOME = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\THETA_LIB";
 
         static void Main(string[] args)
         {
+            ResponseCode res;
+
+            if (!Directory.Exists(APP_HOME))
+            {
+                Directory.CreateDirectory(APP_HOME);
+            }
+
             CmdDataConnection cmdDataConnection = 
                 new CmdDataConnection(IP, PORT);
             EventConnection eventConnection = 
@@ -34,17 +42,28 @@ namespace TestApp
                 }
             }
 
-            // GetObjectInfo
-            cmdDataConnection.operationRequest(
-                DataPhaseInfo.NoDataOrDataInPhase, OperationCode.GetObjectInfo, 3, objHndls[objHndls.Count - 1]);
-            ObjectInfo oi = new ObjectInfo(cmdDataConnection.RecvData);
+            // オブジェクトハンドル毎に取得する
+            string jpegPath;
+            foreach(UInt32 objHndl in objHndls)
+            {
+                // GetObjectInfo
+                res = cmdDataConnection.operationRequest(
+                    DataPhaseInfo.NoDataOrDataInPhase, OperationCode.GetObjectInfo, 3, objHndl);
+                if (res != ResponseCode.OK) continue;
+                ObjectInfo oi = new ObjectInfo(cmdDataConnection.RecvData);
+                if (oi.ObjectFormat != ObjectFormats.EXIF_JPEG) continue;
 
-            // GetObject
-            cmdDataConnection.operationRequest(
-                DataPhaseInfo.NoDataOrDataInPhase, OperationCode.GetObject, 3, objHndls[objHndls.Count - 1]);
-            FileStream fs = new FileStream(@"C:\Users\kouta\Desktop\test.jpg", FileMode.Create, FileAccess.Write);
-            fs.Write(cmdDataConnection.RecvData, 0, cmdDataConnection.RecvData.Length);
-            fs.Close();
+                // すでに画像が取得済みであれば取得しない
+                jpegPath = APP_HOME + "\\" + oi.Filename;
+                if (File.Exists(jpegPath)) continue;
+
+                // GetObject
+                cmdDataConnection.operationRequest(
+                    DataPhaseInfo.NoDataOrDataInPhase, OperationCode.GetObject, 3, objHndl);
+                FileStream fs = new FileStream(jpegPath, FileMode.Create, FileAccess.Write);
+                fs.Write(cmdDataConnection.RecvData, 0, cmdDataConnection.RecvData.Length);
+                fs.Close();
+            }
 
             cmdDataConnection.close();
             eventConnection.close();
